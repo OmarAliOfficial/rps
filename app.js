8ed9a5e97e2fdab77a65a38cbbdd901b0dece19a
Omar <124088826+oali220@users.noreply.github.com>
2023-06-18 15:47:10 -0500

    CSS styling added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 let computerScore = document.querySelector('.cScore');
 let playerScore = document.querySelector('.pScore');
+let update = document.querySelector('.update');
 
 function getComputerChoice(){
     let choices = ["r", "p", "s"];
@@ -27,30 +28,38 @@ scissors.addEventListener('click', () => {
 
 
 function playRound(playerChoice, computerChoice){
-    if(playerChoice == computerChoice)
+    if(playerChoice == computerChoice){
+        update.textContent = "Tie!";
         return "Tie game!";
+    }
     else if(playerChoice == "s" && computerChoice == "r"){
         cScorer();
+        game();
         return "Computer wins!";
     }
     else if(playerChoice == "s" && computerChoice == "p"){
         pScorer();
+        game();
         return "Player wins!";
     }
     else if(playerChoice == "p" && computerChoice == "r"){
         pScorer();
+        game();
         return "Player wins!";
     }
     else if(playerChoice == "p" && computerChoice == "s"){
         cScorer();
+        game();
         return "Computer wins!";
     }
     else if(playerChoice == "r" && computerChoice == "s"){
         pScorer();
+        game();
         return "Player wins!";
     }
     else if(playerChoice == "r" && computerChoice == "p"){
         cScorer();
+        game();
         return "Computer wins!";
     }
 }
@@ -59,17 +68,31 @@ function pScorer(){
     let number = parseInt(playerScore.textContent);
     number++;
     playerScore.textContent = number;
+    update.textContent = "Player wins!";
 }
 
 function cScorer(){
     let number = parseInt(computerScore.textContent);
     number++;
     computerScore.textContent = number;
+    update.textContent = "Computer wins!";
 }
-
-
 
 function game(){
+    let pNum = parseInt(playerScore.textContent);
+    let cNum = parseInt(computerScore.textContent);
+    
+    if (cNum >= 5){
+        update.textContent = "Computer wins out of 5!";
+        console.log("Computer wins out of 5!");
+        return true;
+    }
+    else if (pNum >= 5){
+        update.textContent = "Player wins out of 5!";
+        console.log("Player wins out of 5!");
+        return true;
+    }
+    return false;
 
-    while()
 }
+
