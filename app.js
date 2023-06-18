56c0e7958886184e19d86ef239c3fe14c011061a
Omar <124088826+oali220@users.noreply.github.com>
2023-06-18 14:20:56 -0500

    DOM functionality added

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
 function getComputerChoice(){
     let choices = ["r", "p", "s"];
     return choices[Math.floor(Math.random() * (3 - 0) + 0)];
 }
 
-function playerChoice(){
-    let input = prompt("Choose your move: r for rock, p for paper, s for scissors");
-    return input.toLowerCase();
+function playerChoice(choice){
+    return choice;
 }
+
+rock.addEventListener('click', () => {
+    console.log(playRound("r", getComputerChoice()));
+});
+
+paper.addEventListener('click', () => {
+    console.log(playRound("p", getComputerChoice()));
+});
+
+scissors.addEventListener('click', () => {
+    console.log(playRound("s", getComputerChoice()));
+});
+
 
 function playRound(playerChoice, computerChoice){
     if(playerChoice == computerChoice)
@@ -25,32 +41,34 @@ function playRound(playerChoice, computerChoice){
         return "Computer wins!";
 }
 
+
+
 function game(){
 
-    let pCount = 0;
-    let cCount = 0;
+    // let pCount = 0;
+    // let cCount = 0;
 
-    for(let i = 0; i < 5; i++){
-        let pChoice = playerChoice();
-        let cChoice = getComputerChoice();
+    // for(let i = 0; i < 5; i++){
+    //     let pChoice = playerChoice();
+    //     let cChoice = getComputerChoice();
 
-        if(playRound(pChoice, cChoice) == "Computer wins!"){
-            cCount += 1;
-            console.log("Computer Wins!");
-        }
-        else if(playRound(pChoice, cChoice) == "Player wins!"){
-            pCount += 1;
-            console.log("Player Wins!");
-        }
-        else{
-            console.log("Tie!");
-        }
-    }
+    //     if(playRound(pChoice, cChoice) == "Computer wins!"){
+    //         cCount += 1;
+    //         console.log("Computer Wins!");
+    //     }
+    //     else if(playRound(pChoice, cChoice) == "Player wins!"){
+    //         pCount += 1;
+    //         console.log("Player Wins!");
+    //     }
+    //     else{
+    //         console.log("Tie!");
+    //     }
+    // }
 
-    if(pCount > cCount)
-        console.log("Player wins out 5!");
-    else if (cCount > pCount)
-        console.log("Computer wins out of 5!")
+    // if(pCount > cCount)
+    //     console.log("Player wins out 5!");
+    // else if (cCount > pCount)
+    //     console.log("Computer wins out of 5!")
 }
 
 game();
